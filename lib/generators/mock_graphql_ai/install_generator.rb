fa63a25bb9d36cd34b115ad51bc3506a307edff4
Moiz Ali <amoiz4142@gmail.com>
2023-03-31 14:14:53 +0500

    Add model config in install_generator

--- lib/generators/mock_graphql_ai/install_generator.rb
+++ lib/generators/mock_graphql_ai/install_generator.rb
@@ -12,6 +12,10 @@ module MockGraphqlAi
       def copy_controller_file
         copy_file('graphql_mocks_controller.rb', 'app/controllers/graphql_mocks_controller.rb')
       end
+      
+      def copy_model_file
+        copy_file('mock_graphql_response.rb', 'app/models/mock_graphql_response.rb')
+      end
 
       def register_controller_main_route
         route "get '/imitate_graphql', to: 'graphql_mocks#imitate', as: :imitate_graphql"
